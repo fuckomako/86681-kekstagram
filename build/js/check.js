4d5e5623ea4fd8df91cffb0f3b73c6b1d1370d4c
fuckomako <shnoop87@yandex.ru>
2016-08-04 21:05:06 +0300

    Выполнение второго задания

--- build/js/check.js
+++ build/js/check.js
@@ -1,5 +1,33 @@
 function getMessage(a,b) {
-  if (a = number) {
-    "Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов"
+if (typeof a === 'boolean') {
+  if (a) {
+    return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
   }
+}else {
+return "Переданное GIF-изображение не анимировано";
 }
+
+if (typeof a === 'number') {
+return "Переданное SVG-изображение содержит " + a +" объектов и " + (b * 4)+ " атрибутов";
+}
+
+if (Array.isArray(a)) {
+  var amountOfRedPoints = [];
+  var amountPoints = 0;
+  for (var i = 0; i < amountOfRedPoints.length; i++) {
+    amountPoints += amountOfRedPoints[i];
+  }
+  return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+}
+
+if (Array.isArray(a) && Array.isArray(b)) {
+  var artifactsSquare = [];
+  var amountSquare = 0;
+  for (var i = 0; i < amountOfRedPoints.length; i++) {
+    amountSquare *= artifactsSquare[i];
+  }
+  return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+}
+
+
+};
