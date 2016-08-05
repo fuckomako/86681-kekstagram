ef19a877bb646e2a1c359c459362d851f67a6e60
Fuckomako <shnoop87@yandex.ru>
2016-08-05 19:14:23 +0300

    Сделал наконец то второе задание

--- build/js/check.js
+++ build/js/check.js
@@ -1,33 +1,31 @@
-function getMessage(a,b) {
+function getMessage(a, b) {
 if (typeof a === 'boolean') {
-  if (a) {
+  if (a === true) {
     return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
   }
-}else {
-return "Переданное GIF-изображение не анимировано";
+  if (a === false) {
+  return "Переданное GIF-изображение не анимировано";
+  }
 }
+
 
 if (typeof a === 'number') {
 return "Переданное SVG-изображение содержит " + a +" объектов и " + (b * 4)+ " атрибутов";
 }
 
 if (Array.isArray(a)) {
-  var amountOfRedPoints = [];
-  var amountPoints = 0;
-  for (var i = 0; i < amountOfRedPoints.length; i++) {
-    amountPoints += amountOfRedPoints[i];
+  var amountOfRedPoints = 0;
+  for (var i = 0; i < a.length; i++) {
+    amountOfRedPoints += a[i];
   }
   return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
 }
 
 if (Array.isArray(a) && Array.isArray(b)) {
-  var artifactsSquare = [];
-  var amountSquare = 0;
-  for (var i = 0; i < amountOfRedPoints.length; i++) {
-    amountSquare *= artifactsSquare[i];
+  var artifactsSquare = 0;
+  for (var i = 0; i < a.length && i < b.length; i++) {
+    artifactsSquare *= a[i] + b[i];
   }
   return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
 }
-
-
 };
